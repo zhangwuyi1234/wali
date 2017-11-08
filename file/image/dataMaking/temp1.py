acfefdbd1c7cd0cd2864ed18c2f0dfc21e7c8fc1
zhangwuyi1234 <158507220@qq.com>
2017-11-08 13:58:16 +0800

    Update temp1.py

--- file/image/dataMaking/temp1.py
+++ file/image/dataMaking/temp1.py
@@ -1,1 +1,11 @@
 
+import cPickle as p
+import numpy as np
+import chardet
+def unpickle(file):
+    import cPickle
+    with open(file, 'rb') as fo:
+        dict = cPickle.load(fo)
+    return dict
+cc=unpickle("data_batch_0")
+print(cc)

a6260af771258acf618dd94ea8cf4cfe36a50abb
zhangwuyi1234 <158507220@qq.com>
2017-11-09 14:03:20 +0800

    Update upFolder.py

--- file/image/dataMaking/upFolder.py
+++ file/image/dataMaking/upFolder.py
@@ -32,8 +32,9 @@ def shearFolder(folder,folderPath,c):
         os.remove(tempPath)
         #os.system(' rm -rf '+tempPath) 
 
-folderPath="/data/image"
-folder_ad_Path="/data/toimage"
-imgSize=200
+
 if __name__ == "__main__":
+    folderPath="/data/image"
+    folder_ad_Path="/data/toimage"
+    imgSize=200
     shearFileList(folderPath)

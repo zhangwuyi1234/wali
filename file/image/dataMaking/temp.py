bcc636069871bd0e611b79784d2829ca08634e1c
zhangwuyi1234 <158507220@qq.com>
2017-11-10 18:24:43 +0800

    Update temp.py

--- file/image/dataMaking/temp.py
+++ file/image/dataMaking/temp.py
@@ -9,8 +9,12 @@ data={}
 folder="/data/image"
 folder_ad="/data/toimage"
 imglist=listdir(folder)
+imglist_ad=listdir(folder_ad)
 for i in imglist:
     path=folder+"/"+i
     filelist=listdir(path)
     num=len(filelist)
     print(i+"=="+str(num))
+    
+for j in imglist_ad:
+    print(j)

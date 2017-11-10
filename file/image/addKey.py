116d20169fea39a528a2f712a2f53c6f3076caa2
zhangwuyi1234 <158507220@qq.com>
2017-11-10 18:53:58 +0800

    Update addKey.py

--- file/image/addKey.py
+++ file/image/addKey.py
@@ -15,6 +15,10 @@ for i in images:
     code1=code+"20171106"
     code2=code+"20171107"
     code3=code+"20171108"
+    code4=code+"20171109"
+    code5=code+"20171110"
     db.image.insert({'fileName':code1,'flag':0})
     db.image.insert({'fileName':code2,'flag':0})
     db.image.insert({'fileName':code3,'flag':0})
+    db.image.insert({'fileName':code4,'flag':0})
+    db.image.insert({'fileName':code5,'flag':0})

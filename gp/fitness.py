d6d8845ae40eaa081fba91e8a3b91e89f7ff3f5f
zhangwuyi1234 <158507220@qq.com>
2017-11-17 21:02:41 +0800

    Create fitness.py

---
+++ gp/fitness.py
@@ -0,0 +1,81 @@
+# -*-coding:utf-8-*-
+import requests
+import json
+import codecs
+import io 
+import sys  
+import cPickle as pickle
+
+#889976
+def getName():
+    url = "http://www.iwencai.com/traceback/index/get-query-type-list"
+    r = requests.get(url)
+    d=r.text
+    j=json.loads(d)
+    c=j['data']
+    list=[]
+    for i in c:
+        s=i['subNav']
+        for k in s:
+            name=k['name']
+            str=k['value']
+            for c in str:
+                #list.append(name+u''+c)
+				writ(name+u''+c+';\n')
+				#print(c)
+
+def getData(query):
+    url = "http://www.iwencai.com/regression/back-test-new?query="+query+"&daysForSaleStrategy=2,5,10&startDate=2016-05-26&endDate=2017-11-14&fell=0.001"
+    r = requests.get(url)
+    d=r.text
+    d.encode("utf-8")
+    j=json.loads(d)
+    #list2 = map(msg.get, list1)  
+    #print(list2[0])
+    #a=eval(d)
+    c=j['data']['result']['result']['rate_data']
+    list=[]
+    for i in c:
+        list.append(float(i['winRate']))
+	  fitness=int(max(list)*100)
+    output=str(fitness)+url
+    if fitness>70:
+        dump(output)
+    print(output)
+    return fitness
+#getData()
+
+def dump(data):
+    output = open("dayu70.bin", 'wb')
+    pickle.dump(data, output)
+
+def writ(str):
+    with open('douban.txt','a') as f:
+        for s in str:
+            s = s.encode('utf-8')
+            f.write(s)  
+def read():
+    f=open('douban.txt')
+    text=f.readlines()
+    if text[0][:3] == codecs.BOM_UTF8: 
+        text[0] = text[0][3:] 	
+    list=[]		
+    for i in text:
+        list.append(i.decode("utf-8")[:-1])
+    print(len(list))
+    print(list[-1])
+    return list
+#getName()
+
+
+def run(individual):
+    list=read()
+    temp=""
+    for i in individual:
+        temp+=list[i]
+    query=temp+u"非新股;非ST;非*ST;非停牌;不包含ST;非停牌股;不含次新股;非创业板;上市天数>30日;股价5-45元;"
+    return getData(query)
+#list=[0,2,4,5]
+#print(run(list))	
+
+	

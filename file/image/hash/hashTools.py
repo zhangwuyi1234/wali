da69733a8dbc05c18fde239bf102f091a35357b7
zhangwuyi1234 <158507220@qq.com>
2017-12-01 13:44:33 +0800

    Update hashTools.py

--- file/image/hash/hashTools.py
+++ file/image/hash/hashTools.py
@@ -2,6 +2,7 @@
 from PIL import Image
 import imagehash
 from numpy import *
+from pymongo import *
 
 def enHash(imagePath):
     highfreq_factor = 1
@@ -31,8 +32,34 @@ def similarity(hash1,hash2):
 
 
 if __name__ == "__main__":
-    imagePath1="/home/wuyi/data/toimage/28bird_bullocks_oriole.jpg"
-    imagePath2="/home/wuyi/data/toimage/30bird_bullocks_oriole.jpg"
-    hash1=enHash(imagePath1)
-    hash2=enHash(imagePath2)
-    similarity(hash1,hash2)
+    #imagePath1="/home/wuyi/data/toimage/28bird_bullocks_oriole.jpg"
+    #imagePath2="/home/wuyi/data/toimage/30bird_bullocks_oriole.jpg"
+    #hash1=enHash(imagePath1)
+    #hash2=enHash(imagePath2)
+    #similarity(hash1,hash2)
+    client = MongoClient("localhost", 27017)
+    db = client.share
+    collection = db.code
+    
+    count=db.image.count({'flag':2,'enHash':{$exists:false}})
+    print(count)
+    k=0
+    for i in range(count):
+        i=i+1
+        image=db.image.find({"flag":2,'enHash':{$exists:false}}).skip(k).limit(i)
+        k=i
+        output=str(image[0]['output'])
+        fileName=str(image[0]['fileName'])
+        path="/data/image/"+output+'_'+fileName+".png"
+        isExists=os.path.exists(path)
+        if not isExists:
+            try:
+                db.image.update({'fileName':fileName},{'$set':{'flag':1}})
+            except BaseException as inst:
+                print("updateImageError")
+                db.image.update({'flag':1,'fileName':fileName},{'$set':{'flag':-1}})
+         else:
+            enHash=enHash(path)
+            print(enHash)
+            db.image.update({'fileName':fileName},{'$set':{'enHash':enHash}})
+    upMongo.upShare()

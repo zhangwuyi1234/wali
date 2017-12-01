0104755f41bb5f14800b10fd248befb3a3343beb
zhangwuyi1234 <158507220@qq.com>
2017-12-01 14:55:00 +0800

    Update hashTools.py

--- file/image/hash/hashTools.py
+++ file/image/hash/hashTools.py
@@ -44,12 +44,14 @@ if __name__ == "__main__":
     db = client.share
     collection = db.code
     
-    count=db.image.count({'flag':2,'enHash':{'$exists':False}})
+    count=db.image.count({'flag':2,'enHash':{'$exists':'false'}})
     print(count)
+    if count==0:
+        return
     k=0
     for i in range(count):
         i=i+1
-        image=db.image.find({"flag":2,'enHash':{'$exists':False}}).skip(k).limit(i)
+        image=db.image.find({"flag":2,'enHash':{'$exists':'false'}}).skip(k).limit(i)
         k=i
         output=str(image[0]['output'])
         fileName=str(image[0]['fileName'])

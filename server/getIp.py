06b6d493823696bcaa68e80b11b8147b16612d48
zhangwuyi1234 <158507220@qq.com>
2018-01-14 23:16:00 +0800

    Create getIp.py

---
+++ server/getIp.py
@@ -0,0 +1,9 @@
+def get_out_ip():
+    url = r'http://1212.ip138.com/ic.asp'
+    r = requests.get(url)
+    txt = r.text
+    ip = txt[txt.find("[") + 1: txt.find("]")]
+    print('ip:' + ip)
+    return ip
+
+get_out_ip()
